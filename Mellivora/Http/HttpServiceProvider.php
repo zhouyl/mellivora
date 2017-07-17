036a5bc3bd581db333adf94e7bce439f52609d24
zhouyl <zhouyonglong@jrq.com>
2017-07-17 11:08:38 +0800

    * cookies/request/response 合并到一个 provider 服务

---
+++ Mellivora/Http/HttpServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Mellivora\Http;
+
+use Mellivora\Support\ServiceProvider;
+use Slim\Http\Headers;
+
+class HttpServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerCookiesProvider();
+        $this->registerRequestProvider();
+        $this->registerResponseProvider();
+    }
+
+    /**
+     * 注册 Cookies 服务
+     */
+    public function registerCookiesProvider()
+    {
+
+        $this->container['cookies'] = function ($container) {
+            $config = $container['config']->get('session.cookies');
+
+            $cookies = new Cookies($config->toArray());
+
+            // 数据需要加密处理
+            if ($config->encrypt) {
+                $cookies->setEncryption($container['crypt']);
+            }
+
+            return $cookies;
+        };
+    }
+
+    /**
+     * 注册 Http Request 服务
+     */
+    public function registerRequestProvider()
+    {
+        $this->container['request'] = function ($container) {
+            return Request::createFromEnvironment($container['environment']);
+        };
+    }
+
+    /**
+     * 注册 Http Response 服务
+     */
+    public function registerResponseProvider()
+    {
+        $this->container['response'] = function ($container) {
+            $headers  = new Headers(['Content-Type' => 'text/html; charset=UTF-8']);
+            $response = new Response(200, $headers);
+
+            return $response->withProtocolVersion($container['settings']['httpVersion']);
+        };
+    }
+}
